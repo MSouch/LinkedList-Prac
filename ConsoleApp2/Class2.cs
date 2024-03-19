553121267d4d16c7e48d28914dff2af482d59c38
MSouch <msouch3@gmail.com>
2024-03-19 10:34:55 -0600

    Add project files.

---
+++ ConsoleApp2/Class2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class LinkedList
+    {
+        public Node head;
+
+        public LinkedList()
+        {
+            head = null;
+        }
+        public void InsertAtBeginning(int newdata)
+        {
+            Node newNode = new Node(newdata);
+            newNode.next = head;
+            head = newNode;
+        }
+        public void Displaylist()
+        {
+            Node temp = head;
+            while(temp != null)
+            {
+                Console.WriteLine(temp.data);
+                temp = temp.next;
+            }
+        }
+        public void InsertAtEnd(int newdata)
+        {
+            Node newNode = new Node(newdata);
+            if(head == null)
+            {
+                head = newNode;
+                return;
+            }
+            Node temp = head;
+            while(temp.next != null)
+            {
+                temp = temp.next;
+                if (temp == null)
+                {
+                    temp.next = newNode;
+                    
+                }
+            }
+            
+        }
+
+        public bool Search(int key)
+        {
+            Node current = head;
+            while(current != null)
+            {
+                if (current.data == key)
+                {
+                    return true;
+                }
+                current = current.next;
+            }
+            return false;
+        
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp2
+{
+ class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            
+            Console.WriteLine("Inserting at the beginning of the list");
+            list.InsertAtBeginning(5);
+            list.InsertAtBeginning(10);
+            list.InsertAtBeginning(15);
+            
+            
+            list.Displaylist(); 
+            Console.WriteLine("Inserting at the end of the list");
+            list.InsertAtEnd(20);
+            list.InsertAtEnd(25);
+            list.InsertAtEnd(30);
+            list.Displaylist();
+
+            Console.WriteLine("Searching for elements");
+            Console.WriteLine(list.Search(5));
+            Console.WriteLine(list.Search(20));
+
+        }
+    }
+}
